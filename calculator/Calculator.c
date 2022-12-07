680988fc14e991d6abd1240a18bc0d3057a01a1a
lmrapha <raphaallemos@gmail.com>
2022-12-07 19:33:43 -0300

    Calculator

---
+++ calculator/Calculator.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+
+    char operator;
+    double n1;
+    double n2;
+    double result;
+
+    printf("\nPlease enter an operator! OPTIONS:\n(+ Addition)(- Subtraction)(* Multiplication)(/ Division): ");
+    scanf("%c", &operator);
+
+    printf("Enter Number One: ");
+    scanf("%lf", &n1);
+
+    printf("Enter Number Two: ");
+    scanf("%lf", &n2);
+
+    switch(operator) {
+        default:
+            printf("%c is not a valid operator!", operator);
+            break;
+        case '+':
+            result = n1 + n2;
+            printf("\nResult: %.2lf", result);
+            break;
+        case '-':
+            result = n1 - n2;
+            printf("\nResult: %.2lf", result);
+            break;
+        case '*':
+            result = n1 * n2;
+            printf("\nResult: %.2lf", result);
+            break;
+        case '/':
+            result = n1 / n2;
+            printf("\nResult: %.2lf", result);
+            break;
+    }
+
+    return 0;
+}
