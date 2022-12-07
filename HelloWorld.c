feb367f533a510d26af27cad548ca0a4dc4b7922
lmrapha <raphaallemos@gmail.com>
2022-12-07 14:17:13 -0300

    escape sequence operations

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,9 +3,24 @@
 int main(){
 
 
-    /* \n  = newline
-        \t = tab
-    
+    /* 
+
+    \a = alert
+    \b = backspace
+    \e = escape character
+    \f = formfeed page break
+    \n  = newline (line feed)
+    \t = horizontal tab
+    \v = vertical tab
+    \\ = backslash 
+    \* = apostrophe or single quotation mark
+    \" = double quotation mark
+    \? = question mark (used to avoid trigraphs)
+    \nnn = The byte whose numerical value is given by nnn interpreted as an octal number
+    \xhh…	any	The byte whose numerical value is given by hh… interpreted as a hexadecimal number
+    \uhhhhnote 3	none	Unicode code point below 10000 hexadecimal (added in C99)[1]: 26 
+    \Uhhhhhhhhnote 4	none	Unicode code point where h is a hexadecimal digit
+
     */
 
     printf("Hello world!\nI'm learning C!\n");
