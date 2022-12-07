80039f2805c3b70bc9128a2c82665d182c6294b5
lmrapha <raphaallemos@gmail.com>
2022-12-07 14:08:21 -0300

    grid

--- HelloWorld.c
+++ HelloWorld.c
@@ -10,5 +10,11 @@ int main(){
 
     printf("Hello world!\nI'm learning C!\n");
 
+    // a line with numbers
+    printf("\n1\t2\t3\t4\t5\t6\t7\t8\t9\n");
+
+    // a grid with numbers
+    printf("\n1\t2\t3\n4\t5\t6\n7\t8\t9");
+
     return 0;
 }
