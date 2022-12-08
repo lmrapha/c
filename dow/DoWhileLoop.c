55204a78075bc3ce5d9d943abfae1212dcc84c11
lmrapha <raphaallemos@gmail.com>
2022-12-08 10:35:32 -0300

    Do While Loop

--- dow/DoWhileLoop.c
+++ dow/DoWhileLoop.c
@@ -7,7 +7,19 @@
 
 int main(){
 
-    
+    int number = 0;
+    int sum = 0;
+
+    do {
+        printf("Enter a number above 0: ");
+        scanf("%d", &number);
+
+        if(number > 0) {
+            sum += number;
+        }
+    } while(number > 0);
+
+    printf("sum: %d", sum);
 
     return 0;
 }
