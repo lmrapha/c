b4f8538314ed96bc856576dc0c9e0edbe5386c6d
lmrapha <raphaallemos@gmail.com>
2022-12-08 11:17:03 -0300

    Array print

--- array/Array.c
+++ array/Array.c
@@ -7,16 +7,34 @@
 
 int main(){
 
-    double prices[5]; //{5.0, 10.0, 15.0, 20.0, 25.0};
+    //double prices[5]; //{5.0, 10.0, 15.0, 20.0, 25.0};
+    double prices[] = {5.0, 10.0, 15.0, 20.0, 25.0, 40.0, 30.0};
 
+    /*
     prices[0] = 5.0;
     prices[1] = 10.0;
     prices[2] = 15.0;
     prices[3] = 20.0;
     prices[4] = 25.0;
-    prices[5] = 30.0;
+    prices[5] = 60.0;
+    */
 
-    printf("$%.2lf", prices[5]);
+    //fixed value
+    //printf("$%.2lf", prices[5]);
+    
+    //printf("%d bytes \n", sizeof(prices));
+
+    //print the array size
+    for(int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++) {
+        printf("$%.2lf\n", prices[i]);
+    }
+
+    // fixed value of array
+    /*
+    for(int i = 0; i < 5; i++) {
+        printf("$%.2lf\n", prices[i]);
+    }
+    */
 
     return 0;
 }
