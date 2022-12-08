0832721151cc8c9362f282d6265f8b07d986e887
lmrapha <raphaallemos@gmail.com>
2022-12-08 10:17:55 -0300

    Loops

---
+++ for/ForLoop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+*   Learning C Exercise
+*   @lmrapha
+*/
+
+int main(){
+
+    
+
+    return 0;
+}
---
+++ nested/NestedLoop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+*   Learning C Exercise
+*   @lmrapha
+*/
+
+int main(){
+
+    int rows;
+    int columns;
+    char symbol;
+
+    printf("\nEnter # number of rows: ");
+    scanf("%d", &rows);
+
+
+    return 0;
+}
