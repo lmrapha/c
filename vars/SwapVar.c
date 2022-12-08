d0c35a453340b56b765ac4cb188c7e5dfd46c49f
lmrapha <raphaallemos@gmail.com>
2022-12-08 13:28:02 -0300

    Swapping Values

--- vars/SwapVar.c
+++ vars/SwapVar.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 /*
-*   Learning C Exercise
-*   @lmrapha
-*/
+ *   Learning C Exercise
+ *   @lmrapha
+ */
 
-int main(){
+int main()
+{
 
-    
+    char x[15] = "wine";
+    char y[15] = "water";
+    char temp[15];
+
+    strcpy(temp, x);
+    strcpy(x, y);
+    strcpy(y, temp);
+
+    /*
+        temp = x;
+        x = y;
+        y = temp;
+    */
+
+    printf("x = %s\n", x);
+    printf("y = %s\n", y);
 
     return 0;
 }
