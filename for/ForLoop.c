1aec9adf0c41eb60228986b608bdda89aecd6938
lmrapha <raphaallemos@gmail.com>
2022-12-08 10:30:07 -0300

    For Loop

--- for/ForLoop.c
+++ for/ForLoop.c
@@ -7,7 +7,13 @@
 
 int main(){
 
-    
+    for(int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    for(int j = 1; j <= 10; j++) {
+        printf("\nmeow");
+    }
 
     return 0;
 }
