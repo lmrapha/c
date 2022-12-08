6d51072c0d5ee050fda790d2cc637af23769089e
lmrapha <raphaallemos@gmail.com>
2022-12-08 10:25:00 -0300

    Nested Loops

--- nested/NestedLoop.c
+++ nested/NestedLoop.c
@@ -14,6 +14,21 @@ int main(){
     printf("\nEnter # number of rows: ");
     scanf("%d", &rows);
 
+    printf("\nEnter # number of columns: ");
+    scanf("%d", &columns);
+
+    scanf("%c");
+
+    printf("\nEnter a symbol to use: ");
+    scanf("%c", &symbol);
+
+    for(int i = 1; i <= rows; i ++) {
+       
+        for(int j = 1; j <= columns; j++) {
+            printf("%c", symbol);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
